a6fa006878d526187f025d0217d88f3d181eb9e5
fnagelkop <feru.nagel@gmail.com>
2017-05-29 22:23:03 -0300

    prueba insert polimorfico

---
+++ Objetos/Automovil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberFrba.Objetos
+{
+    class Automovil
+    {
+       public Automovil(string turno, string patente,string modelo,string marca,string chofer) {
+            this.turno = turno;
+            this.patente = patente;
+            this.modelo = modelo;
+            this.marca = marca;
+            this.chofer = chofer;
+        }
+        public string turno { get; set; }
+        public string patente { get; set; }
+        public string modelo{ get; set; }
+        public string marca { get; set; }
+        public string chofer{ get; set; }
+    }
+    }
