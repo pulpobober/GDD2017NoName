4e279b97188ea4e972e3c8b8a802fabaf6fb7f8d
Ivan Bober <ivibober@gmail.com>
2017-06-10 19:42:54 -0300

    Facturacion cliente parte grafica nada mas

--- Facturacion/FacturacionClientes.Designer.cs
+++ Facturacion/FacturacionClientes.Designer.cs
@@ -28,11 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnFacturacion = new System.Windows.Forms.Button();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.cmbClientes = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnFacturacion
+            // 
+            this.btnFacturacion.Location = new System.Drawing.Point(378, 13);
+            this.btnFacturacion.Name = "btnFacturacion";
+            this.btnFacturacion.Size = new System.Drawing.Size(205, 66);
+            this.btnFacturacion.TabIndex = 0;
+            this.btnFacturacion.Text = "button1";
+            this.btnFacturacion.UseVisualStyleBackColor = true;
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Location = new System.Drawing.Point(12, 18);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(47, 13);
+            this.lblCliente.TabIndex = 1;
+            this.lblCliente.Text = "Clientes:";
+            // 
+            // cmbClientes
+            // 
+            this.cmbClientes.FormattingEnabled = true;
+            this.cmbClientes.Location = new System.Drawing.Point(65, 15);
+            this.cmbClientes.Name = "cmbClientes";
+            this.cmbClientes.Size = new System.Drawing.Size(287, 21);
+            this.cmbClientes.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(571, 153);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // FacturacionClientes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(595, 357);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.cmbClientes);
+            this.Controls.Add(this.lblCliente);
+            this.Controls.Add(this.btnFacturacion);
+            this.Name = "FacturacionClientes";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnFacturacion;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.ComboBox cmbClientes;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
